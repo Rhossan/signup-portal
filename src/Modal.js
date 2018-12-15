69bc9c43ee382b13277dd8eff035427deb2466ae
Rhossan <rhossan93@gmail.com>
2018-12-15 18:32:46 -0500

    initial setup and commit

---
+++ src/Modal.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import Modal from 'react-modal';
+
+
+const customStyles = {
+    content: {
+        top: '50%',
+        left: '50%',
+        right: 'auto',
+        bottom: 'auto',
+        backgroundColor: 'red'
+    }
+};
+
+
